8b3003e7a255495b336a7a446d29662f9a1dac27
christopher-laclair <chrislaclair@gmail.com>
2020-07-14 14:28:12 -0700

    changed you lose message

--- kisses/script.js
+++ kisses/script.js
@@ -91,7 +91,7 @@ gameOver = status => {
   if (status === 'win') {
     startButton.innerHTML = 'You win! Play again?';
   } else {
-    startButton.innerHTML = 'You lose, go pick up Mini poops!';
+    startButton.innerHTML = 'Try Again - Go pick up Mini poops!';
   }
   currentlyPlaying = false;
 };
